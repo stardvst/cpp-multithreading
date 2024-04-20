2881617c1b05012220392ca6cdcddd3125ff80ba
stardvst <lilia.manukyan@hotmail.com>
2024-04-20 08:46:48 +0400

    std::timed_mutex example

---
+++ ch13-unique-lock/timed-mutex.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <string>
+#include <mutex>
+
+using namespace std::literals;
+
+std::timed_mutex mutex;
+
+void task1()
+{
+  std::cout << "task1 trying to get lock" << std::endl;
+  mutex.lock();
+  std::cout << "task1 has lock" << std::endl;
+
+  // pretend to be busy
+  std::this_thread::sleep_for(500ms);
+
+  std::cout << "task1 releasing lock" << std::endl;
+  mutex.unlock();
+}
+
+void task2()
+{
+  std::this_thread::sleep_for(100ms);
+
+  std::cout << "task2 trying to get lock" << std::endl;
+  while (!mutex.try_lock_for(100ms))
+    std::cout << "task2 couldn't get lock" << std::endl;
+
+  std::cout << "task2 has lock" << std::endl;
+
+  // pretend to be busy
+  std::this_thread::sleep_for(200ms);
+
+  std::cout << "task2 releasing lock" << std::endl;
+  mutex.unlock();
+}
+
+int main()
+{
+  std::thread t1{task1}, t2{task2};
+
+  t1.join();
+  t2.join();
+}
