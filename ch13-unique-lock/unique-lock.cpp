1925d85d8d1c6464785b17cafa55a547a44e0b06
stardvst <lilia.manukyan@hotmail.com>
2024-04-20 08:41:02 +0400

    using std::unique_lock

---
+++ ch13-unique-lock/unique-lock.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <string>
+#include <mutex>
+
+using namespace std::literals;
+
+// must be outside thread functions
+std::mutex printMutex;
+
+void print(const std::string &str)
+{
+  for (int i = 0; i < 5; i++)
+  {
+    std::unique_lock lk(printMutex);
+    std::cout << str[0] << str[1] << str[2] << std::endl;
+    lk.unlock();
+
+    std::this_thread::sleep_for(50ms);
+  }
+}
+
+int main()
+{
+  std::thread t1{print, "abc"};
+  std::thread t2{print, "def"};
+  std::thread t3{print, "xyz"};
+
+  t1.join();
+  t2.join();
+  t3.join();
+}
